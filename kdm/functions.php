cc5084b5f98e31fb0903d1ee271ceed8d4863a8f
Privatebeamss <scriptingace@gmail.com>
2025-10-06 19:33:47 +0200

    Update functions.php

--- kdm/functions.php
+++ kdm/functions.php
@@ -9,6 +9,7 @@ add_action('after_setup_theme', 'hobea_theme_setup');
 
 function hobea_theme_scripts() {
   wp_enqueue_style('hobea-style', get_stylesheet_uri());
+  wp_enqueue_script('hobea-menu-toggle', get_template_directory_uri() . '/menu-toggle.js', array(), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'hobea_theme_scripts');
 ?>
